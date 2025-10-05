69e810a6709f05539bd516652d1580967d856fde
Himanshuu23 <sagarhimanshu472@gmail.com>
2025-10-05 19:11:11 +0530

    added LSTM

--- multi_class_classification/model.py
+++ multi_class_classification/model.py
@@ -20,4 +20,130 @@ test_dir = os.path.join(dataset_dir, "test")
 batch_size = 32
 seed = 42
 
-raw_train_ds = tf.keras.utils.dataset
+raw_train_ds = tf.keras.utils.text_dataset_from_directory(
+    train_dir,
+    batch_size=batch_size,
+    validation_split=0.2,
+    subset="training",
+    seed=seed)
+
+raw_val_ds = tf.keras.utils.text_dataset_from_directory(
+    train_dir,
+    batch_size=batch_size,
+    validation_split=0.2,
+    subset="validation",
+    seed=seed)
+
+raw_test_ds = tf.keras.utils.text_dataset_from_directory(
+    test_dir,
+    batch_size=batch_size)
+
+def custom_standardization(input_data):
+  lowercase = tf.strings.lower(input_data)
+  stripped_html = tf.strings.regex_replace(lowercase, '<br />', ' ')
+  return tf.strings.regex_replace(stripped_html,
+                                  '[%s]' % re.escape(string.punctuation),
+                                  '')
+
+max_features = 10000
+sequence_length = 250
+
+vectorize_layer = layers.TextVectorization(
+    standardize=custom_standardization,
+    max_tokens=max_features,
+    output_mode='int',
+    output_sequence_length=sequence_length)
+
+train_text = raw_test_ds.map(lambda x, y: x)
+vectorize_layer.adapt(train_text)
+
+def vectorize_text(text, label):
+  text = tf.expand_dims(text, -1)
+  return vectorize_layer(text), label
+
+train_ds = raw_train_ds.map(vectorize_text)
+val_ds = raw_val_ds.map(vectorize_text)
+test_ds = raw_test_ds.map(vectorize_text)
+
+AUTOTUNE = tf.data.AUTOTUNE
+
+train_ds = train_ds.cache().prefetch(buffer_size=AUTOTUNE)
+val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+test_ds = test_ds.cache().prefetch(buffer_size=AUTOTUNE)
+
+embedding_dim = 64
+
+model = tf.keras.Sequential([
+  layers.Embedding(max_features, embedding_dim),
+  layers.Dropout(0.2),
+  layers.Bidirectional(layers.LSTM(64)),
+  layers.Dropout(0.2),
+  layers.Dense(10, activation='softmax')])
+
+model.summary()
+
+model.compile(loss=losses.SparseCategoricalCrossentropy(),
+              optimizer='adam',
+              metrics=[tf.metrics.SparseCategoricalAccuracy()])
+
+epochs = 20
+history = model.fit(
+    train_ds,
+    validation_data=val_ds,
+    epochs=epochs)
+
+loss, accuracy = model.evaluate(test_ds)
+
+print("Loss: ", loss)
+print("Accuracy: ", accuracy)
+
+print("Class names:", raw_train_ds.class_names)
+
+history = model.fit(
+    train_ds,
+    validation_data=val_ds,
+    epochs=epochs)
+
+loss, accuracy = model.evaluate(test_ds)
+print("Loss:", loss)
+print("Accuracy:", accuracy)
+
+history_dict = history.history
+print("History keys:", history_dict.keys())
+
+acc = history_dict['sparse_categorical_accuracy']
+val_acc = history_dict['val_sparse_categorical_accuracy']
+loss = history_dict['loss']
+val_loss = history_dict['val_loss']
+
+epochs_range = range(1, len(acc) + 1)
+
+plt.plot(epochs_range, loss, 'bo', label='Training loss')
+plt.plot(epochs_range, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.show()
+
+plt.plot(epochs_range, acc, 'bo', label='Training acc')
+plt.plot(epochs_range, val_acc, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+plt.legend(loc='lower right')
+plt.show()
+
+export_model = tf.keras.Sequential([
+    vectorize_layer,
+    model
+])
+
+export_model.compile(
+    loss=losses.SparseCategoricalCrossentropy(from_logits=False),
+    optimizer="adam",
+    metrics=['accuracy']
+)
+
+metrics = export_model.evaluate(raw_test_ds, return_dict=True)
+print(metrics)
