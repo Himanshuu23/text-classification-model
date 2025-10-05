ef6721e443e419a198637c72ad321d35f5d648ae
Himanshuu23 <sagarhimanshu472@gmail.com>
2025-10-05 14:58:53 +0530

    evaluating the model

--- model.py
+++ model.py
@@ -10,9 +10,92 @@ from tensorflow.keras import losses
 
 URL = "https://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz"
 
-dataset = tf.keras.utils.get_file("aclImdb_v1", URL,
+dataset_dir = tf.keras.utils.get_file("aclImdb_v1", URL,
                                     untar=True, cache_dir='.',
                                     cache_subdir='')
+aclImdb_dir = os.path.join(dataset_dir, "aclImdb")
+train_dir = os.path.join(aclImdb_dir, 'train')
+test_dir = os.path.join(aclImdb_dir, 'test')
+remove_dir = os.path.join(train_dir, 'unsup')
+shutil.rmtree(remove_dir)
 
-dataset_dir = os.path.join(os.path.dirname(dataset), 'aclImdb')
-train_dir = os.path.join(dataset_dir, 'train')
+batch_size = 32
+seed = 42
+
+raw_train_ds = tf.keras.utils.text_dataset_from_directory(
+    train_dir,
+    batch_size=batch_size,
+    validation_split=0.2,
+    subset='training',
+    seed=seed)
+
+raw_val_ds = tf.keras.utils.text_dataset_from_directory(
+    train_dir,
+    batch_size=batch_size,
+    validation_split=0.2,
+    subset='validation',
+    seed=seed)
+
+raw_test_ds = tf.keras.utils.text_dataset_from_directory(
+    test_dir,
+    batch_size=batch_size)
+
+def custom_standardization(input_data):
+  lowercase = tf.strings.lower(input_data)
+  stripped_html = tf.strings.regex_replace(lowercase, '<br />', ' ')
+  return tf.strings.regex_replace(stripped_html,
+                                  '[%s]' % re.escape(string.punctuation),
+                                  '')
+
+max_features = 10000
+sequence_length = 250
+
+vectorize_layer = layers.TextVectorization(
+    standardize=custom_standardization,
+    max_tokens=max_features,
+    output_mode='int',
+    output_sequence_length=sequence_length)
+
+train_text = raw_train_ds.map(lambda x, y: x)
+vectorize_layer.adapt(train_text)
+
+def vectorize_text(text, label):
+  text = tf.expand_dims(text, -1)
+  return vectorize_layer(text), label
+
+train_ds = raw_train_ds.map(vectorize_text)
+val_ds = raw_val_ds.map(vectorize_text)
+test_ds = raw_test_ds.map(vectorize_text)
+
+AUTOTUNE = tf.data.AUTOTUNE
+
+train_ds = train_ds.cache().prefetch(buffer_size=AUTOTUNE)
+val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+test_ds = test_ds.cache().prefetch(buffer_size=AUTOTUNE)
+
+embedding_dim = 16
+
+model = tf.keras.Sequential([
+  layers.Embedding(max_features, embedding_dim),
+  layers.Dropout(0.2),
+  layers.GlobalAveragePooling1D(),
+  layers.Dropout(0.2),
+  layers.Dense(1, activation='sigmoid')])
+
+model.summary()
+
+model.compile(loss=losses.BinaryCrossentropy(),
+              optimizer='adam',
+              metrics=[tf.metrics.BinaryAccuracy(threshold=0.5)])
+
+epochs = 10
+history = model.fit(
+    train_ds,
+    validation_data=val_ds,
+    epochs=epochs)
+
+loss, accuracy = model.evaluate(test_ds)
+
+print("Loss: ", loss)
+print("Accuracy: ", accuracy)
+
